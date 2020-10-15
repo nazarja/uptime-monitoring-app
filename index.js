812ed14368ada86be597d016c2c3fbb131ecfec2
Sean Murphy <mail.seanmurphy@gmail.com>
2020-10-15 12:48:29 +0100

    refactor index / server js files

--- index.js
+++ index.js
@@ -1,73 +1,13 @@
-const config = require('./lib/config');
-const http = require('http');
-const https = require('https');
-const url = require('url');
-const fs = require('fs');
-const { StringDecoder } = require('string_decoder');
-const handlers = require('./lib/handlers');
-const helpers = require('./lib/helpers');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-/*=============================
-  Router
-=============================*/
+const app = {};
 
-const router = {
-    'users': handlers.users,
-    'tokens': handlers.tokens,
-    'checks': handlers.checks,
-    'ping': handlers.ping
+app.init = () => {
+    server.init();
+    workers.init();
 };
 
-/*=============================
-  HTTP Server
-=============================*/
+app.init();
 
-const httpServer = http.createServer((req, res) => server(req, res));
-const httpsServerOptions = { key: fs.readFileSync('./https/key.pem'), cert: fs.readFileSync('./https/cert.pem') };
-const httpsServer = https.createServer(httpsServerOptions, (req, res) => server(req, res));
-
-const server = (req, res) => {
-    const parsedURL = url.parse(req.url, true);
-    const path = parsedURL.pathname.replace(/^\/+|\/+$/g, '');
-    const query = parsedURL.query;
-    const method = req.method.toLowerCase();
-    const headers = req.headers;
-    let buffer = '';
-    let decoder = new StringDecoder('utf-8');
-
-    req.on('data', data => buffer += decoder.write(data));
-    req.on('end', () => {
-        buffer += decoder.end();
-
-        const routehandler = typeof (router[path]) !== 'undefined'
-            ? router[path]
-            : handlers.notfound
-
-        const data = {
-            path,
-            query,
-            method,
-            headers,
-            payload: helpers.parseJsonToObject(buffer)
-        };
-
-        routehandler(data, (statusCode, payload) => {
-            console.log(data)
-            statusCode = typeof (statusCode) === 'number' ? statusCode : 200;
-            payload = typeof (payload) === 'object' ? payload : {};
-            payload = JSON.stringify(payload);
-
-            res.writeHead(statusCode);
-            res.end(payload);
-            console.log('RESPONSE: ', statusCode, payload);
-        });
-    });
-};
-
-
-/*=============================
-   Port Listener
-=============================*/
-
-httpServer.listen(config.port.http, () => console.log(`Server listening on: http://localhost:${config.port.http}/ | ${config.env}`));
-httpsServer.listen(config.port.https, () => console.log(`Server listening on: https://localhost:${config.port.https}/ | ${config.env}`));
+module.exports = app;
